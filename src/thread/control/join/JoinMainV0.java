c6e22b3ca95d38e68ff85a5192f3586ea094c85a
유열 <vflag21c@nate.com>
2024-10-12 23:26:26 +0900

    Java Thread - 스레드 제어와 생명주기

---
+++ src/thread/control/join/JoinMainV0.java
@@ -0,0 +1,26 @@
+package thread.control.join;
+
+
+import static uitl.MyLogger.log;
+import static uitl.ThreadUtils.sleep;
+
+public class JoinMainV0 {
+    public static void main(String[] args) {
+        log("Start");
+        Thread thread1 = new Thread(new Job(), "thread-1");
+        Thread thread2 = new Thread(new Job(), "thread-2");
+
+        thread1.start();
+        thread2.start();
+        log("end");
+    }
+
+    static class Job implements Runnable {
+        @Override
+        public void run() {
+            log("작업 시작");
+            sleep(2000);
+            log("작업 완료");
+        }
+    }
+}
