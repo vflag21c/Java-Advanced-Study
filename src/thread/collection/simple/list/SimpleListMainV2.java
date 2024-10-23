05abdce0eb073a684c2461b37ac2d6b5d33c89c8
유열 <vflag21c@nate.com>
2024-10-23 23:04:32 +0900

    Java Thread - 동시성 컬렉션

--- src/thread/collection/simple/list/SimpleListMainV2.java
+++ src/thread/collection/simple/list/SimpleListMainV2.java
@@ -5,7 +5,8 @@ import static util.MyLogger.log;
 public class SimpleListMainV2 {
     public static void main(String[] args) throws InterruptedException {
 //        test(new BasicList());
-        test(new SyncList());
+//        test(new SyncList());
+        test(new SyncProxyList(new BasicList()));
     }
 
     private static void test(SimpleList list) throws InterruptedException {
